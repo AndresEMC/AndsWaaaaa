4adae5ebc3f1dd9e031f93a48b0db74d06e71481
AndresEMC <andresmorales316@aragon.unam.mx>
2020-10-15 17:44:44 -0500

    AYURA

---
+++ pila.py
@@ -0,0 +1,17 @@
+pila=[3,4,5]   #pila de inicio con un arreglo
+print (pila) #imprescion de la pila 
+
+pila.append(7)
+pila.append(8)
+
+print (pila)
+
+#34578
+print ("Saco este elemento", pila.pop())
+
+#8
+
+print("Se quedo asi la pila:" , pila)
+#3457
+
+
